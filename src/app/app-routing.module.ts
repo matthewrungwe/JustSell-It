b24c0e41761ecb927e71d448a95697e2c3feb8b3
Jamila Mustapha <jamilamustapha14@gmail.com>
2020-10-16 13:30:33 +0100

    added login and about page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,26 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
+      {
+        path: '',
+        // If this path is the 'full' match...
+        pathMatch: 'full',
+        // ...redirect to this route.
+        redirectTo: 'home',
+      },
       // Here we will add our application pages
       {
         path: 'home',
         loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
       },
+      {
+        path: 'about',
+        loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule),
+      },
     ],
   },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
 ];
 
 @NgModule({
