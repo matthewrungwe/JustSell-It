3f83dbcb3440398bb9fdba4f5aaaeebcda5e0124
Godwin Maga <godwinmaga6@gmail.com>
2020-10-18 16:31:52 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,30 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './ui/layout/layout.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        // If this path is the 'full' match...
-        pathMatch: 'full',
-        // ...redirect to this route.
-        redirectTo: 'home',
-      },
-      // Here we will add our application pages
-      {
-        path: 'home',
-        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
-      },
-      {
-        path: 'about',
-        loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule),
-      },
-    ],
-  },
-  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+    {
+      path: '',
+      component: LayoutComponent,
+      children: [
+        {
+          path: '',
+          // If this path is the 'full' match...
+          pathMatch: 'full',
+          // ...redirect to this route.
+          redirectTo: 'home',
+        },
+        // Here we will add our application pages
+        { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+        { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+      ],
+    },
+    { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+    { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
 ];
 
 @NgModule({
