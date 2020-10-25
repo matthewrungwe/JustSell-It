e530d5cdc96f149e737bf04e6d1945bbfec456b2
Godwin Maga <godwinmaga6@gmail.com>
2020-10-26 00:11:19 +0100

    Create Landingpage-routing.module.ts

---
+++ src/app/pages/landingpage/Landingpage-routing.module.ts
@@ -0,0 +1,13 @@
+import { LandingpageComponent } from './landingpage.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const routes: Routes = [{ path: '', component: LandingpageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LandingpageRoutingModule { }
