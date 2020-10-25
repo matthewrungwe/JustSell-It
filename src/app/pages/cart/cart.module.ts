2b75274a939de9c5bb7edc3edb535c057073578f
Godwin Maga <godwinmaga6@gmail.com>
2020-10-26 00:10:55 +0100

    Create cart.module.ts

---
+++ src/app/pages/cart/cart.module.ts
@@ -0,0 +1,13 @@
+import { CartRoutingModule } from './cart-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CartRoutingModule,
+  ]
+})
+export class CartModule { }
